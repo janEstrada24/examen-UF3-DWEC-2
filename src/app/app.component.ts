6d042adce04379b2ac142db62f07bab7a78d527f
janEstrada24 <jestrada48@boscdelacoma.cat>
2023-03-13 18:23:31 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'examen-UF3-DWEC-2';
+}
